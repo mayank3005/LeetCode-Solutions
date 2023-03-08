5d93d36ed2de433065550e764369f37a89f7dbd4
Mayank <82897488+mayank3005@users.noreply.github.com>
2023-03-08 11:06:03 +0530

    Added solution - LeetHub

--- Sum of Middle Elements of two sorted arrays - GFG/sum-of-middle-elements-of-two-sorted-arrays.cpp
+++ Sum of Middle Elements of two sorted arrays - GFG/sum-of-middle-elements-of-two-sorted-arrays.cpp
@@ -10,35 +10,30 @@ using namespace std;
 
 class Solution {
 public:
-    int findMidSum(int ar1[], int ar2[], int n) {
+    int findMidSum(int nums1[], int nums2[], int n) {
             // code here 
-            int i=0,j=0;
-            int prev=n-1,after=n;
-            int prevEle,afterEle;
-            while(i<n and j<n){
-                int elem;
-                if(ar1[i]<ar2[j]){
-                    elem=ar1[i++];
-                }else{
-                    elem=ar2[j++];
-                }
-                if(i+j-1==prev) prevEle=elem;
-                if(i+j-1==after) afterEle=elem;
-                // cout<<i+j-1<<' '<<elem<<endl;
+        int n1=n,n2=n;
+        int l=0,r=n;
+        while(l<=r){
+            int cut1=(l+r)>>1;
+            int cut2=(n1+n2)/2-cut1;
+            
+            int l1,l2,r1,r2;
+            l1=l2=INT_MIN,r1=r2=INT_MAX;
+            if(cut1) l1=nums1[cut1-1];
+            if(cut2) l2=nums2[cut2-1];
+            if(cut1!=n1) r1=nums1[cut1];
+            if(cut2!=n2) r2=nums2[cut2]; 
+            
+            if(l1<=r2 and l2<=r1){
+                return max(l1,l2)+min(r2,r1);
             }
-            while(i<n){
-                int elem=ar1[i++];
-                if(i+j-1==prev) prevEle=elem;
-                if(i+j-1==after) afterEle=elem;
-                // cout<<i+j-1<<' '<<elem<<endl;
-            }
-            while(j<n){
-                int elem=ar2[j++];
-                if(i+j-1==prev) prevEle=elem;
-                if(i+j-1==after) afterEle=elem;
-                // cout<<i+j-1<<' '<<elem<<endl;
-            }
-            return prevEle+afterEle;
+            
+            if(l1>r2) r=cut1-1;
+            else l=cut1+1;
+            
+        }
+        return 0;
     }
 };
 
