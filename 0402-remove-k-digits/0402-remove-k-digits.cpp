2af4bc64a6fad9f2e95fa55c0bff69e9fa0ff14f
Mayank <82897488+mayank3005@users.noreply.github.com>
2023-04-21 10:12:58 +0530

    Time: 10 ms (82.74%), Space: 8.6 MB (70.16%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        int n=num.size();
-        vector<vector<int>> suff(n+1,vector<int>(10,-1));
-        for(int i=n-2;i>=0;i--){
-            for(int j=0;j<10;j++){
-                suff[i][j]=suff[i+1][j];
+        string res;
+        int keep = num.size() - k;
+        for (int i=0; i<num.size(); i++) {
+            while (res.size()>0 && res.back()>num[i] && k>0) {
+                res.pop_back();
+                k--;
             }
-            suff[i][num[i+1]-'0']=i+1;
+            res.push_back(num[i]);
         }
-        int i=0,j; string ans="";
-        while(i<n){
-            for(int s=0;s<num[i]-'0';s++){
-                j=suff[i][s];
-                if(j==-1) continue;
-                if(j-i<=k) {k-=(j-i);i=j;break;}
-            }
-            // cout<<i<<" "<<k<<endl; 
-            ans.push_back(num[i++]);
-        }
-        while(k-- >0 and ans.size()) ans.pop_back();
-        reverse(ans.begin(),ans.end());
-        while(ans.size()>1 and ans.back()=='0') ans.pop_back();
-        reverse(ans.begin(),ans.end());
-        return ans.size()?ans:"0";
+        res.erase(keep, string::npos);
+        
+        // trim leading zeros
+        int s = 0;
+        while (s<(int)res.size()-1 && res[s]=='0')  s++;
+        res.erase(0, s);
+        
+        return res=="" ? "0" : res;
     }
 };
