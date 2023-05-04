59102b82a7402f86047a353e18d39dc8ebdc137d
Mayank <82897488+mayank3005@users.noreply.github.com>
2023-05-04 13:52:37 +0530

    Time: 8 ms (45.49%), Space: 8.5 MB (99.28%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& v) {
+        int n=v.size();
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<v[i].size();j++){
+                v[i][j]+=min(v[i+1][j],v[i+1][j+1]);
+            }
+        }
+        return v[0][0];
+    }
+};
