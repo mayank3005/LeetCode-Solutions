3f36757f3e6dcc78277a60724a474979bcc67d96
Mayank <82897488+mayank3005@users.noreply.github.com>
2023-09-03 07:50:58 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (78.55%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+    int dp[101][101];
+    
+    int f(int i,int j){
+        if(i==0 or j==0) return 1;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        return dp[i][j]=f(i-1,j)+f(i,j-1);
+        
+    }
+    
+    int uniquePaths(int m, int n) {
+        
+        memset(dp,-1,sizeof(dp));
+        return f(m-1,n-1);
+        
+    }
+};
